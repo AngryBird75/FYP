4ba78d3ae18e9ff7e71faa97eca133f493af7a3a
AngryBird75 <pgcnaturequeen1122@gmail.com>
2026-07-03 14:10:24 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+using AspiraHub.Data;
+using AspiraHub.Repositories;
+using AspiraHub.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<ICareerService, CareerService>();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(60);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+    options.Cookie.Name = ".AspiraHub.Session";
+});
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<ISkillCatalogService, SkillCatalogService>();
+// ── DI ──────────────────────────────────────────
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IEmailService, EmailService>();
+builder.Services.AddScoped<UniqueKeyService>();
+builder.Services.AddScoped<IDashboardRepository, DashboardRepository>();
+// Add this line with your other service registrations
+builder.Services.AddScoped<IDegreeService, DegreeService>();
+builder.Services.AddScoped<IRoadmapService, RoadmapService>();
+builder.Services.AddScoped<IJobService, JobService>();
+builder.Services.AddScoped<ILearningService, LearningService>();
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseSession();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Auth}/{action=Index}/{id?}");
+
+app.Run();
